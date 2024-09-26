0d47cb8826146bb4a1eab691af4f9464a56402fd
AntonyCalPoly <atartako@calpoly.edu>
2024-09-26 15:52:16 -0700

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -7,11 +7,11 @@ router = APIRouter()
 
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
-   with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
     """
     Each unique item combination must have only a single price.
     """
+   with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
 
     return [
             {
