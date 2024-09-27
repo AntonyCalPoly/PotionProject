17a5f9d4ebe4dd9771addde81a280dcda65e281a
AntonyCalPoly <atartako@calpoly.edu>
2024-09-27 17:22:37 +0000

    changed

--- src/api/carts.py
+++ src/api/carts.py
@@ -110,6 +110,6 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
     with db.engine.begin() as connection:
         inventory = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = " + str(inventory[2] - 1) + ", gold = " + str(inventory[4] + 50)))
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = " + str(inventory[0] - 1) + ", gold = " + str(inventory[2] + 50)))
 
     return {"total_potions_bought": 1, "total_gold_paid": 50}
