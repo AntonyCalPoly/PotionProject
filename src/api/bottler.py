a8a873dc4c0a7864a82a0485e60007f17a44aebb
AntonyCalPoly <atartako@calpoly.edu>
2024-09-30 17:14:13 +0000

    updated bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -42,7 +42,7 @@ def get_bottle_plan():
 
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).first()[0]
-
+        result = result_cursor.fetchone()
         quantity = int(result // 100)
 
     return [
