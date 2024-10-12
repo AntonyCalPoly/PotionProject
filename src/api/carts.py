020e7ab79f2fb504817e32d6f9c0409e0c0762b4
AntonyCalPoly <atartako@calpoly.edu>
2024-10-12 16:42:20 -0700

    updated cart

--- src/api/carts.py
+++ src/api/carts.py
@@ -111,11 +111,11 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     with db.engine.begin() as connection:
         price_of_potions = connection.execute(sqlalchemy.text(f"SELECT cost, num_potions FROM global_inventory WHERE sku = '{item_sku}';")).fetchone()
         current_cart = connection.execute(sqlalchemy.text(f"SELECT quantity,payment FROM cart WHERE cart_id = {cart_id};")).fetchone()
-        if current_cart.quantity == 0:
-            new_payment = cart_item.quantity * 50  
-        else:
-            new_payment = cart_item.quantity * 50  
-            total_quantity = cart_item.quantity
+        
+        total_quantity = cart_item.quantity if current_cart.quantity == 0 else current_cart.quantity + cart_item.quantity
+        
+        new_payment = total_quantity * 50 
+
         connection.execute(sqlalchemy.text(f"UPDATE cart SET quantity = {total_quantity}, payment = {new_payment} WHERE cart_id = {cart_id};"))
         connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_potions = num_potions - {cart_item.quantity} WHERE sku = '{item_sku}';"))
 
