35f35eea84c66cc8ba013d5ade7ba5707fda8d5a
AntonyCalPoly <atartako@calpoly.edu>
2024-10-14 19:29:15 -0700

    removed commented out code

--- src/api/carts.py
+++ src/api/carts.py
@@ -112,19 +112,6 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
 
     return "OK"
 
-    '''
-    current_cart = connection.execute(sqlalchemy.text(f"SELECT quantity, payment FROM cart WHERE cart_id = {cart_id};")).fetchone()
-        
-        if current_cart is None:
-            
-            total_quantity = cart_item.quantity
-        else:
-            
-            total_quantity = current_cart.quantity + cart_item.quantity
-
-        new_payment = total_quantity * 50  
-    '''
-
 
 class CartCheckout(BaseModel):
     payment: str
